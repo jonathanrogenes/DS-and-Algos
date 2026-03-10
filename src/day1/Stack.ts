c6cb430c9f76c0b74bc0c7f9b483535eb7463d8d
Jonathan Rogenes <jonathanrogenes@gmail.com>
2026-03-10 12:13:00 +0900

    Stack again!

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -10,26 +10,27 @@ export default class Stack<T> {
 
 
     constructor() {
-      this.head = undefined;
-      this.length = 0;
+      this.head = undefined
+      this.length = 0
     }
 
 
     push(item: T): void {
-      const node = {value: item} as Node<T>;
+      const node = {value: item} as Node<T>
 
       this.length++
       if (!this.head) {
         this.head = node;
         return
       }
-
       node.prev = this.head
       this.head = node
 }
 
     pop(): T | undefined {
       this.length = Math.max(0, this.length - 1)
+
+     
       if (this.length === 0) {
         const head = this.head
         this.head = undefined
@@ -37,9 +38,7 @@ export default class Stack<T> {
       }
       const head = this.head as Node<T>
       this.head = head.prev
-
       return head.value
-
 }
     peek(): T | undefined {
       return this.head?.value
