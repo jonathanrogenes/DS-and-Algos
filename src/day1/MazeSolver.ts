54bee7e66e8028bffbebf45e6245a140f28feacc
Jonathan Rogenes <jonathanrogenes@gmail.com>
2026-03-11 15:03:10 +0900

    Base case for recursion. Maze problem

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,27 @@
+function walk(maze:string[], wall: string, curr: Point, end: Point, seen: boolean[][]) {
+  //1 base case
+  // off the map
+  if (curr.x < 0 || curr.x >= maze[0].length ||
+      curr.y < 0 || curr.y >= maze.length) {
+        return false
+      }
+
+  // on a wall
+  if (maze[curr.y][curr.x] === wall) {
+    return false
+  }
+
+  // if at end
+  if (curr.x === end.x && curr.y === end.y) {
+    return false
+  }
+
+  // if seen before
+  if (seen[curr.x][curr.y]) {
+    return false
+  }
+}
+
 export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
 
 }
