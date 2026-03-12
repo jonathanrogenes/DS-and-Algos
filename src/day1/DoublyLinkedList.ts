55bf4a42041cd55683d844dfc018a15c0cd16900
Jonathan Rogenes <jonathanrogenes@gmail.com>
2026-03-12 13:09:36 +0900

    doubly liked list implemented with all methods

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -41,11 +41,7 @@ export default class DoublyLinkedList<T> {
       }
 
       this.length++
-      let curr = this.head
-      for (let i = 0; curr && idx < this.length; i++) {
-        curr = curr.next
-      }
-      curr = curr as Node<T>
+      const curr = this.getAt(idx) as Node<T>
       const node = {value: item} as Node<T>
 
       node.next = curr
@@ -69,12 +65,61 @@ export default class DoublyLinkedList<T> {
       this.tail = node
 }
     remove(item: T): T | undefined {
+      let curr = this.head
+      for (let i = 0; curr && i < this.length; i++) {
+        if (curr.value === item) {
+          break
+        }
+        curr = curr.next
+      }
 
+      if (!curr) {
+        return undefined
+      }
+
+      return this.removeNode(curr)
 }
     get(idx: number): T | undefined {
-
+      return this.getAt(idx)?.value
 }
     removeAt(idx: number): T | undefined {
+      const node = this.getAt(idx)
 
+      if (!node) {
+        return undefined
+      }
+      return this.removeNode(node)
+}
+private removeNode(node: Node<T>): T | undefined {
+      this.length--
+      if (this.length === 0) {
+        const out = this.head?.value
+        this.head = this.tail = undefined
+        return out
+      }
+
+      if (node.prev) {
+        node.prev = node.next
+      }
+
+      if (node.next) {
+        node.next = node.prev
+      }
+
+      if (node === this.head) {
+        this.head = node.next
+      }
+      if (node === this.tail) {
+        this.tail = node.prev
+      }
+      node.prev = node.next = undefined
+      return node.value
+}
+private getAt(idx: number): Node<T> | undefined {
+  let curr = this.head
+  for (let i = 0; curr && i < idx; i++) {
+    curr = curr.next
+  }
+  return curr
 }
 }
